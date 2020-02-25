9c76b4986de4b40751a529a5a12842cfd1037730
Yossi Shor <54455502+yosshor@users.noreply.github.com>
2020-02-25 11:35:11 +0200

    Update try_open_file.py

--- try_open_file.py
+++ try_open_file.py
@@ -1,17 +1,13 @@
-
+"""
+author : Yossi Shor
+"""
 
 with open('new_text.txt', 'r') as p:
     print( min([l for l in p.read().split('\n')], key = len))
     print([line for line in p.read().split('\n') if len(line) == min([l for l in p.read().split('\n')], key = len)])
     print([len(line) for line in p.read().split('\n')])# if len(line) == len(sorted(p.read().split('\n'),key=len)[0])])
     print([line for line in p.read().split('\n') if len(line)==len(sorted(p.read().split('\n'),key=len)[0])])
-    
-    
-    
-    
-    
-    
-    
+   
     
     
 print("now you import the {} script".format(__name__))
