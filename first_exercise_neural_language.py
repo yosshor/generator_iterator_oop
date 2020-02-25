4cc024cd377f6c3e6598910bb932811a23a453a0
Yossi Shor <54455502+yosshor@users.noreply.github.com>
2020-02-25 10:54:36 +0200

    Update first_exercise_neural_language.py

--- first_exercise_neural_language.py
+++ first_exercise_neural_language.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
 """
-Created on Tue Dec 24 20:45:33 2019
-
 @author: EGLOBAL
 """
 
@@ -15,6 +12,13 @@ download('stopwords')
 from nltk.stem.porter import PorterStemmer 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+
 
 t = re.sub('[^a-zA-Z1-9]','')
 dataset = pd.read_csv(r'C:\src\P14-Natural-Language-Processing\Natural_Language_Processing\Restaurant_Reviews.tsv', delimiter = '\t')
@@ -38,9 +42,7 @@ X.shape
 Y = dataset.iloc[:,1].values
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 x = X
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+
 
 random_Classifier = RandomForestClassifier(criterion = 'entropy', n_estimators = 10, random_state = 0)
 random_Classifier.fit(x_train, y_train)
@@ -67,11 +69,9 @@ classifier = GaussianNB()
 classifier.fit(x_train, y_train)
 y_pred = classifier.predict(x_test)
 
-from sklearn.metrics import confusion_matrix
 confu = confusion_matrix(y_test, y_pred)
 print((confu[0][0] + confu[1][1]) / 250)
 
-from sklearn.metrics import accuracy_score
 accu = accuracy_score(y_test, y_pred)
 print(accu)
 
